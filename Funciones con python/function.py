f6b2ef5669c2f3b84390b37b48b21b15313c1484
Julian Cruz <freakcruz@me.com>
2015-10-25 16:30:49 -0500

    Se realiza primer commit al repositorio python

---
+++ Funciones con python/function.py
@@ -0,0 +1,29 @@
+#Declarando una Funcion
+#Funcion que dice Hola
+def di_hola():
+	print("Hola mundo")
+	
+
+
+#Llamando la Funcion
+di_hola()
+
+#Funcion que saluda laguna persona
+def saludame(nombre):
+	print("Hola {}".format(nombre))
+
+
+#Llamando la funcion Saludame
+saludame("Julian")
+
+#Funcion que agrega el numero 2
+def agregar_dos(numero):
+	return numero + 2
+
+#Llamando la funcion para agregar el numero 2
+resultado = agregar_dos(2)
+
+
+
+
+
